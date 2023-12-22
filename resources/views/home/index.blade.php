e264af07bf4935fdf173d3c564d4c69ca0b2c4ce
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-12-22 03:07:46 +0100

    imagens adicionadas no inicio

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,16 +1,70 @@
 <?php include "../resources/views/include/head.blade.php"; ?>
 
 <main>
-    <div class="container">
+    <div class="container d-flex align-items-center" style="min-height: inherit">
         <div class="row">
             <div class="col">
-                <h1>Seja Bem-vindo ao nosso sistema</h1>
+                <h1 class="pt-4 pb-4">Seja Bem-vindo ao nosso sistema</h1>
                 <dl>
                     <dt>Diagnóstico</dt>
-                    <dd>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, omnis! Fugit non, quod consequuntur veritatis sapiente magni provident, harum sit porro, quam minus mollitia perspiciatis doloribus! Quaerat cumque possimus explicabo.</dd>
-                
+                    <dd>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, omnis! Fugit non, quod
+                        consequuntur veritatis sapiente magni provident, harum sit porro, quam minus mollitia
+                        perspiciatis doloribus! Quaerat cumque possimus explicabo.
+                        <div class="container pt-4">
+                            <div class="row">
+                                <div class="col-8 col-md-3 pt-4">
+                                    <img src="../assets/img/img1.jpg" class="img-fluid " width="100%"
+                                        alt="foto não reconhecida" srcset="">
+                                </div>
+
+                                <div class="col-8 col-md-3 pt-4">
+                                    <img src="../assets/img/img2.jpg" class="img-fluid " width="100%"
+                                        alt="foto não reconhecida" srcset="">
+                                </div>
+
+                                <div class="col-8 col-md-3 pt-4">
+                                    <img src="../assets/img/img3.jpg" class="img-fluid " width="100%"
+                                        alt="foto não reconhecida" srcset="">
+                                </div>
+
+                                <div class="col-8 col-md-3 pt-4">
+                                    <img src="../assets/img/img3.jpg" class="img-fluid " width="100%"
+                                        alt="foto não reconhecida" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                    </dd>
+
                     <dt>Tratamento</dt>
-                    <dd>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, omnis! Fugit non, quod consequuntur veritatis sapiente magni provident, harum sit porro, quam minus mollitia perspiciatis doloribus! Quaerat cumque possimus explicabo.</dd>
+                    <dd>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, omnis! Fugit non, quod
+                        consequuntur veritatis sapiente magni provident, harum sit porro, quam minus mollitia
+                        perspiciatis doloribus! Quaerat cumque possimus explicabo.
+                        <div class="container pt-4">
+                            <div class="row">
+                                <div class="col-8 col-md-3 pt-4">
+                                    <img src="../assets/img/img1.jpg" class="img-fluid " width="100%"
+                                        alt="foto não reconhecida" srcset="">
+                                </div>
+
+                                <div class="col-8 col-md-3 pt-4">
+                                    <img src="../assets/img/img2.jpg" class="img-fluid " width="100%"
+                                        alt="foto não reconhecida" srcset="">
+                                </div>
+
+                                <div class="col-8 col-md-3 pt-4">
+                                    <img src="../assets/img/img3.jpg" class="img-fluid " width="100%"
+                                        alt="foto não reconhecida" srcset="">
+                                </div>
+
+                                <div class="col-8 col-md-3 pt-4">
+                                    <img src="../assets/img/img3.jpg" class="img-fluid " width="100%"
+                                        alt="foto não reconhecida" srcset="">
+                                </div>
+                            </div>
+                        </div>
+                    </dd>
                 </dl>
             </div>
         </div>
